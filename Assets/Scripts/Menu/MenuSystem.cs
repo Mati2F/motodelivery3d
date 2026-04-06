685d8e0b3dbfc9dc030930ef264a144ed3854dc1
mig08 <ibanezmg03@gmail.com>
2026-04-06 00:14:15 -0400

    menu_juego

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+
+    public void Creditos(){
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+}
